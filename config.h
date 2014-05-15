867cbb57b93318a370d291a10caa0575f68825b4
nuschnuschi <nuschnuschi@gmail.com>
2014-05-16 03:58:00 +0900

    Update config.h
    Modified for Lenovo Thinkpad T440s Elan Microelectronics TouchScreen

--- config.h
+++ config.h
@@ -13,8 +13,8 @@
 
 #define NUM_TOUCHES 1
 
-#define TOUCH_VID 0x596
-#define TOUCH_PID 0x524
+#define TOUCH_VID 0x04f3
+#define TOUCH_PID 0x0224
 
 #define SCREEN_RESX 1920
 #define SCREEN_RESY 1080
